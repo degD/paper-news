5da2abd7382d8305091d857a040501a47bc9e637
degD <108623049+degD@users.noreply.github.com>
2024-01-02 23:02:29 +0300

    Improve DateInfo class

--- PaperNews/src/dege/papernews/DateInfo.java
+++ PaperNews/src/dege/papernews/DateInfo.java
@@ -3,13 +3,21 @@ package dege.papernews;
 public class DateInfo {
 	
 	private final int startMonth, startYear;
-	private final int endMonth;
+	private final int endMonth, endYear;
 	
 	public DateInfo(int startMonth, int startYear) {
-		// Months are between 0-11 (inclusive) 
+		// Months are between 1-12
 		this.startMonth = startMonth;
 		this.startYear = startYear;
-		this.endMonth = (startMonth + 1) % 12;
+		
+		if (startMonth == 1) {
+			this.endMonth = 12;
+			this.endYear = startYear;
+		}
+		else {
+			this.endMonth = startMonth - 1;
+			this.endYear = startYear + 1;
+		}
 	}
 	
 	public String getStartDate() {
@@ -18,6 +26,22 @@ public class DateInfo {
 	
 	public String getEndDate() {
 		return String.format("%d/%d", endMonth, startYear + 1);
+	}
+
+	public int getStartMonth() {
+		return startMonth;
+	}
+
+	public int getStartYear() {
+		return startYear;
+	}
+
+	public int getEndMonth() {
+		return endMonth;
+	}
+
+	public int getEndYear() {
+		return endYear;
 	}
 
 	@Override
